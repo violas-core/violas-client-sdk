a6245f9e72618d6a84394a5a3ee31a76b0ae33b5
HunterSun2018 <sht@palliums.org>
2020-10-16 16:36:32 +0800

    support Violas v0.4.0

--- release_install.sh
+++ release_install.sh
@@ -3,10 +3,10 @@ echo "install all binaries to" $1
 mkdir $1 $1/bin $1/lib/ $1/move -p
 echo "created all dirs."
 
-cp rust/client-proxy/target/release/libclient_proxy.so $1/lib/
+cp rust/violas-client/target/release/libviolas_client.so $1/lib/
 echo "copied libclient_proxy.so ."$1"/lib"
 
-cp rust/oracle-service/target/release/violas-oracle $1/bin
+cp rust/violas-oracle/target/release/violas-oracle $1/bin
 echo "copied violas-oracle to "$1"/bin"
 
 cp build/release/lib $1 -r
@@ -17,7 +17,7 @@ echo "copied build/release/bin to "$1"/bin"
 
 cp ../Violas/target/release/cli $1/bin
 cp ../Violas/target/release/libra-node $1/bin
-cp ../Violas/target/release/config-builder $1/bin
+#cp ../Violas/target/release/config-builder $1/bin
 echo "copied ../Violas/target/release/bin to "$1"/bin"
 
 strip $1/lib/*.so $1/bin/* 
